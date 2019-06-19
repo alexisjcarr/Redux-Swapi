588cc18f80bd96318fea28515d69b3dbd0691c86
alexisjcarr <acarr59@gmail.com>
2019-06-18 22:58:35 -0500

    Async action creator coded

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,27 @@
-import axios from 'axios';
+import axios from "axios";
 
 const FETCH_CHARACTERS_START = "FETCH_CHARACTERS_START";
 const FETCH_CHARACTERS_SUCCESS = "FETCH_CHARACTERS_SUCCESS";
 const FETCH_CHARACTERS_FAILURE = "FETCH_CHARACTERS_FAILURE";
 
-// our action creator will be a function that returns a function
-// the url to fetch characters from is `https://swapi.co/api/people/`
-// remember that now we have controll over our thunk-based action creator
-
 export const getCharacters = () => dispatch => {
-    dispatch({
-        type: FETCH_CHARACTERS_START,
+  dispatch({
+    type: FETCH_CHARACTERS_START
+  });
+  axios
+    .get("https://swapi.co/api/people/")
+    .then(res => {
+      console.log(res);
+      dispatch({
+        type: FETCH_CHARACTERS_SUCCESS,
+        payload: res.data
+      });
     })
-}
+    .catch(err => {
+      console.log(err);
+      dispatch({
+        type: FETCH_CHARACTERS_FAILURE,
+        payload: err
+      });
+    });
+};
