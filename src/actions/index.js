6f9254b8e76a425ed7d8595618a0feb8298ae6d5
alexisjcarr <acarr59@gmail.com>
2019-06-18 22:51:42 -0500

    Initial commit. Imported thunk and logger. Wrapped them in compose

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,15 @@
-// we'll need axios
+import axios from 'axios';
 
-// we'll need to create 3 different action types here.
-// one for fetching, one for success and one for failure
+const FETCH_CHARACTERS_START = "FETCH_CHARACTERS_START";
+const FETCH_CHARACTERS_SUCCESS = "FETCH_CHARACTERS_SUCCESS";
+const FETCH_CHARACTERS_FAILURE = "FETCH_CHARACTERS_FAILURE";
 
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+
+export const getCharacters = () => dispatch => {
+    dispatch({
+        type: FETCH_CHARACTERS_START,
+    })
+}
